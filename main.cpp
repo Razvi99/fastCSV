ec60e97fe4e5fda4a13b75c64fa8bf02262f67e2
Razvi99 <razvi.david@gmail.com>
2020-09-21 13:16:34 +0300

    update main to use new features

--- main.cpp
+++ main.cpp
@@ -10,15 +10,20 @@ int main() {
 
     auto start = std::chrono::steady_clock::now();
 
-    int i = 0;
+    fastCSV->skipRow(); // skips header
+    auto second_row = fastCSV->getRow(); // gets current row (2nd)
+    std::string_view first_col = second_row[-2]; // access second to last column of row
+    std::string_view second_row_string = second_row.getRaw(); // get the whole row as a string
+
+    std::cerr << first_col << "\n" << second_row_string << "\n";
+
     for (auto row : *fastCSV) {
-        if (row[0] == "-1") // std::string_view supports == between strings
-            i++;
+        if (row[2] == "ColumnText") // std::string_view supports == between strings
+            std::cerr << row.getRaw();
     }
 
     auto end = std::chrono::steady_clock::now();
 
-    std::cerr << i << "\n";
     printf("total: %f ms\n", (double) std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1e+6);
 
     return 0;
